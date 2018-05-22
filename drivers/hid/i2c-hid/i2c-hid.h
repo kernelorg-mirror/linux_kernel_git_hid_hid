f10e31fbb744a123dae7b30b0bdfcf978ee58b92
Julian Sax <jsbc@gmx.de>
2018-05-22 21:54:27 +0200

    HID: i2c-hid: override HID descriptors for certain devices
    A particular touchpad (SIPODEV SP1064) refuses to supply the HID descriptors.
    This patch provides the framework for overriding these descriptors based on DMI
    data. It also includes the descriptors for said touchpad, as well as the DMI
    data for the 4 laptops known to use this device.
    
    Relevant Bug: https://bugzilla.redhat.com/show_bug.cgi?id=1526312
    
    Cc: Hans de Goede <hdegoede@redhat.com>
    Reported-and-tested-by: ahormann@gmx.net
    Reported-and-tested-by: Bruno Jesus <bruno.fl.jesus@gmail.com>
    Reported-and-tested-by: Dietrich <enaut.w@googlemail.com>
    Reported-and-tested-by: kloxdami@yahoo.com
    Signed-off-by: Julian Sax <jsbc@gmx.de>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Acked-By: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

---
+++ drivers/hid/i2c-hid/i2c-hid.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef I2C_HID_H
+#define I2C_HID_H
+
+
+#ifdef CONFIG_DMI
+struct i2c_hid_desc *i2c_hid_get_dmi_i2c_hid_desc_override(void);
+char *i2c_hid_get_dmi_hid_report_desc_override(unsigned int *size);
+#else
+static inline struct i2c_hid_desc *i2c_hid_get_dmi_i2c_hid_desc_override(void)
+{ return NULL; }
+static inline char *i2c_hid_get_dmi_hid_report_desc_override(unsigned int *size)
+{ return NULL; }
+#endif
+
+#endif
